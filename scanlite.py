4cbc21240ce41609d6c1cdcd69931109f5d7aa6a
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-04-03 20:08:48 +0200

    Create scanlite.py

---
+++ scanlite.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+import sys
+import socket
+
+
+print()
+print(" ____   ____    _    _   _   _ _ _       ")
+print("/ ___| / ___|  / \  | \ | | | (_) |_ ___ ")
+print("\___ \| |     / _ \ |  \| | | | | __/ _ \ ")
+print(" ___) | |___ / ___ \| |\  | | | | ||  __/")
+print("|____/ \____/_/   \_\_| \_| |_|_|\__\___|")
+print()
+
+print("Herramienta creada por S3RGI09 (Sergio Casero Verdial)")
+print("------------------------------------------------------")
+objetivo= socket.gethostbyname(input("Inserte la direccion IP: "))
+print("------------------------------------------------------")
+
+print("Escaneando objetivo: " + objetivo)
+
+try:
+	for port in range(1,150):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		resultado = s.connect_ex((objetivo, port))
+		if resultado == 0:
+			print("El puerto {} está abierto".format(port))
+		s.close()
+except:
+	print("\n Saliendo...")
+	sys.exit(0)
